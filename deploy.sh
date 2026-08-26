cf36ffd1f1ac1701ed47c12d312125398570262c
Gerald Mirilla <gmirilla@gmail.com>
2026-08-26 13:28:04 +0100

    chore: update deploy script to use rsync for public assets and fix echo statement

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 cd ~/newTax
 git pull origin main
-cp -r public/images ~/public_html/images
+rsync -a --exclude='storage' public/ ~/public_html/
 cp -r storage/app/public ~/public_html/storage
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-echo "Deploy complete."php
+echo "Deploy complete."
